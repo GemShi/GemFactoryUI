9923038a1647033852b2bebb7f9216d3f60537bc
GemShi <gemshi@GemShideMacBook-Pro.local>
2017-02-05 10:53:07 +0800

    Initial Commit

---
+++ GemFactoryUI/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GemFactoryUI
+//
+//  Created by GemShi on 2017/2/5.
+//  Copyright © 2017年 GemShi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
